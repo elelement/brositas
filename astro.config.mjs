a264b6895201b29e5e911a8d72d4cdc4c0f3bde9
cabreracanal <jmccanal@pm.me>
2026-08-01 19:40:24 +0200

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: process.env.SITE ?? "https://atelier-ko-topaz.vercel.app",
+  site: process.env.SITE ?? "https://brositas.hackarte.es",
   integrations: [sitemap()],
 });
